03becd9105188273eaf9b47e4f6894d7906005c9
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-11-18 16:13:02 -0700

    Small script change.

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -4,13 +4,12 @@
 #SBATCH --ntasks=20   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=4096M   # memory per CPU core
-#SBATCH -J "CRSP Betas Flow"   # job name
+#SBATCH -J "Get Data"   # job name
 #SBATCH --mail-user=amh1124@byu.edu   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
 
-# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 source .venv/bin/activate
 python research/pipelines
